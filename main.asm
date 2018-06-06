08bfce45cc130970407cf7d5e7ee8d304588dedb
ChrisFoster4 <chrismarkfoster@yahoo.co.uk>
2018-06-06 18:41:17 +0100

    Fixed trailing chars and added user input.

--- main.asm
+++ main.asm
@@ -4,22 +4,34 @@
 section .text
     global _start
 
+section .bss
+    ;Reserving bytes of space for the user input
+    userInput resb USER_STRING_LENGTH
 
 section .data
-        string db `fooo?`,0
         lineThrough db `\314\266`
         buffer db ``
 
 _start:
-    mov r8,0
+    call _getInput
     call _printString
-    exit 0
+    exit 0 ;Macro from macros.include
 
 _printString:
-    strikeChar r8
-    _printChar
-    inc r8
-    mov r9b,[string+r8]
-    cmp r9b,0
-    jne _printString
+    mov r8,0 ;Set the loop counter to 0
+    .loop:
+        strikeChar r8
+        _printBufferChar
+        inc r8                 ;Increment loop counter
+        mov r9b,[userInput+r8] ;Moving one byte of the user input. One byte contains one char.
+        cmp r9b,0
+        jne .loop
+    ret
+
+_getInput:
+    mov rax,SYS_IN
+    mov rdi,STDIN
+    mov rsi,userInput
+    mov rdx,USER_STRING_LENGTH
+    syscall
     ret
